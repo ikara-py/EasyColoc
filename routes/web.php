eeef66e59c8451412ac6416b06ecf79b4d7ef6d2
ali kara <ali16kara@gmail.com>
2026-02-27 05:04:45 +0000

    feat: add colocation create view and routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocations.create');
     Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+    Route::post('/colocations/generate-invite', [ColocationController::class, 'generateInvite'])->name('colocations.generate_invite');
     
+    Route::post('/colocations/join', [ColocationController::class, 'join'])->name('colocations.join');
+    Route::post('/colocations/generate-invite', [ColocationController::class, 'generateInvite'])->name('colocations.generate_invite');
+    Route::delete('/colocations/deactivate', [ColocationController::class, 'deactivate'])->name('colocations.deactivate');
+    Route::delete('/colocations/members/{user}', [ColocationController::class, 'removeMember'])->name('colocations.remove_member');
 });
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
