bb9195665d4c996ec5dc7c1c2d4f5f3f9230651c
ali kara <ali16kara@gmail.com>
2026-02-27 03:50:58 +0000

    feat: update input, button, and label components styling

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block text-[#FE9677] text-xs font-black uppercase mb-2']) }}>
     {{ $value ?? $slot }}
 </label>
